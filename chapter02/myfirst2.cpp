1ed8d6947338e614de29b9616fb95c2fe5c3fce8
roger <sunsan2020@163.com>
2025-03-24 07:23:04 +0800

    Learning disable using namespace std :)

---
+++ chapter02/myfirst2.cpp
@@ -0,0 +1,11 @@
+// myfirst2.cpp -- disable using namespace std :)
+
+#include<iostream>										// a PREPROCESSOR directive
+int main()												// function header
+{														// start of function body
+	// using namespace std;								// make definitions visible
+	std::cout << "Come up and C++ me some time.";		// message
+	std::cout << std::endl;								// start a new line
+	std::cout << "You won't regret it!" << std::endl;	// more output	
+	return 0;											// terminate main()
+}

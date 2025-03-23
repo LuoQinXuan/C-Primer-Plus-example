52503fff28ab08db21690b5c11a24ae9239cf431
roger <sunsan2020@163.com>
2025-03-24 07:35:54 +0800

    using std::function

---
+++ chapter02/myfirst3.cpp
@@ -0,0 +1,12 @@
+// myfirst3.cpp -- just use the using std:: function :)
+
+#include<iostream>										// a PREPROCESSOR directive
+int main()												// function header
+{														// start of function body
+	using std::cout;
+	using std::endl;
+	cout << "Come up and C++ me some time.";		// message
+	cout << endl;								// start a new line
+	cout << "You won't regret it!" << endl;	// more output	
+	return 0;											// terminate main()
+}

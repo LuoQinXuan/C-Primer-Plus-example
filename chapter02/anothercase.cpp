38563a3a35a6dda8ed10922aa1ccabd1da4cf333
roger <sunsan2020@163.com>
2025-03-24 08:02:09 +0800

    add another case :)

---
+++ chapter02/anothercase.cpp
@@ -0,0 +1,12 @@
+// anothercase.cpp -- the necessary ' ' 
+#include <iostream>
+int main()
+{
+	using std::cout;
+	using std::endl;
+	cout << "The Good, the";
+	cout << "Bad, ";
+	cout << "and the Ukulele";
+	cout << endl;
+	return 0;
+}

2176e869364a28ef91a4a28610589a247b699bcd
roger <sunsan2020@163.com>
2025-03-24 23:32:39 +0800

    modify the comment

--- chapter02/myfirst4.cpp
+++ chapter02/myfirst4.cpp
@@ -1,11 +1,11 @@
 // myfirst4.cpp -- cout : input the string to the output stream 
 
-#include<iostream>								// a PREPROCESSOR directive
-int main()										// function header
-{												// start of function body
-	using namespace std;						// make definitions visible
+#include<iostream>					// a PREPROCESSOR directive
+int main()						// function header
+{							// start of function body
+	using namespace std;				// make definitions visible
 	cout << "Come up and C++ me some time.";	// message
-	// cout << endl;							// start a new line
+	// cout << endl;				// start a new line
 	cout << "You won't regret it!" << endl;		// more output	
-	return 0;									// terminate main()
+	return 0;					// terminate main()
 }
